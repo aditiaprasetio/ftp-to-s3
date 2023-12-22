1e54c58d430f49a2a1dceec3631d103c17acc46b
Aditia Prasetio <aditia.prasetio12@gmail.com>
2023-12-22 17:10:34 +0700

    adjust script

--- apps/ftp-to-s3/src/modules/f3ToS3/s3Uploader.service.ts
+++ apps/ftp-to-s3/src/modules/f3ToS3/s3Uploader.service.ts
@@ -5,6 +5,7 @@ import { CronJob } from 'cron';
 import { S3Service } from './s3.service';
 import * as fs from 'fs';
 
+const CRON_FILE_FIX_ISSUE = 'CRON_FILE_FIX_ISSUE';
 const CRON_S3_UPLOADER = 'CRON_S3_UPLOADER';
 
 @Injectable()
@@ -15,15 +16,40 @@ export class S3UploaderSchedulerService {
     private s3Service: S3Service,
   ) {}
 
+  @Cron(CronExpression.EVERY_SECOND, { name: CRON_FILE_FIX_ISSUE })
+  handleCronFixFile() {
+    this.logger.debug('handleCronFixFile');
+    this.handleCronTryCatchFixFile();
+  }
+
   @Cron(CronExpression.EVERY_SECOND, { name: CRON_S3_UPLOADER })
   handleCron() {
-    this.logger.debug('handleCronTryCatch');
+    this.logger.debug('handleCron');
     this.handleCronTryCatch();
   }
 
   restartJob(job: CronJob) {
     this.logger.log('Job restarted');
     job.stop();
+    job.start();
+  }
+
+  async handleCronTryCatchFixFile() {
+    this.logger.log(
+      '==== SCHEDULER START : Process ==== api v' + appJSON.version,
+    );
+    const job = this.schedulerRegistry.getCronJob(CRON_FILE_FIX_ISSUE);
+    job.stop();
+
+    try {
+      await this.processFixFile(job);
+    } catch (err) {
+      this.logger.error('PROCESS FAILED', err);
+    }
+
+    this.logger.log(
+      '==== SCHEDULER END : Process ==== api v' + appJSON.version,
+    );
     job.start();
   }
 
@@ -44,6 +70,79 @@ export class S3UploaderSchedulerService {
       '==== SCHEDULER END : Process ==== api v' + appJSON.version,
     );
     job.start();
+  }
+
+  // from uploaded to upper folder
+  async processFixFile(job: CronJob) {
+    const localDestDir = process.env.DOWNLOADED_DIR + '/uploaded';
+    const list = fs.readdirSync(localDestDir);
+    const countFiles = list.length;
+    let remainingFiles = list.length;
+    let totalProcessedFile = 0;
+    let totalNotFile = 0;
+    this.logger.log('processFixFile - total files: ' + countFiles);
+
+    const willProcessList = list.filter((_, index) => index < 500);
+    const countFilesCurrentProcess = willProcessList.length;
+    let remainingFilesCurrentProcess = willProcessList.length;
+    this.logger.log(
+      'processFixFile - total files (will process): ' +
+        remainingFilesCurrentProcess,
+    );
+
+    for (const idx in willProcessList) {
+      let fileName = willProcessList[idx];
+      if (!fileName.includes('.')) {
+        totalNotFile++;
+        continue;
+      }
+
+      if (!fileName.includes(',')) {
+        totalNotFile++;
+        continue;
+      }
+
+      this.logger.log(
+        `-> ${totalProcessedFile}/${remainingFilesCurrentProcess}/${countFiles}`,
+      );
+
+      totalProcessedFile++;
+
+      if (!fs.existsSync(localDestDir)) {
+        fs.mkdirSync(localDestDir);
+      }
+      const localDestFile = localDestDir + '/' + fileName;
+
+      try {
+        // rename file
+        const newFileName = fileName.replace(/,/g, '-');
+        const localSrcFile = process.env.DOWNLOADED_DIR + '/' + newFileName;
+        fileName = newFileName;
+
+        fs.copyFileSync(localDestFile, localSrcFile);
+        // fs.unlinkSync(localDestFile);
+
+        remainingFiles--;
+        remainingFilesCurrentProcess--;
+        
+        this.logger.log(
+          'processFixFile - (CURRENT) Remaining: ' +
+            remainingFilesCurrentProcess +
+            ' ( ' +
+            (
+              (remainingFilesCurrentProcess / countFilesCurrentProcess) *
+              100
+            ).toFixed(2) +
+            ' % )',
+        );
+        this.logger.log('processFixFile - (OVERALL) Remaining: ' + remainingFiles);
+      } catch (err) {
+        this.logger.error(
+          'processFixFile - ERROR localDestFile: ' + localDestFile,
+        );
+        this.logger.error(err);
+      }
+    }
   }
 
   async process(job: CronJob) {
@@ -73,10 +172,13 @@ export class S3UploaderSchedulerService {
       if (fileName.includes(',')) {
         // rename file
         const newFileName = fileName.replace(/,/g, '-');
-        fs.renameSync(localSrcFile, process.env.DOWNLOADED_DIR + '/' + newFileName)
+        fs.renameSync(
+          localSrcFile,
+          process.env.DOWNLOADED_DIR + '/' + newFileName,
+        );
         fileName = newFileName;
       }
-      
+
       this.logger.log(
         `-> ${totalProcessedFile}/${remainingFilesCurrentProcess}/${countFiles}`,
       );
@@ -84,7 +186,7 @@ export class S3UploaderSchedulerService {
       totalProcessedFile++;
       const key = process.env.STORAGE_DIRECTORY + '/' + fileName;
       this.logger.log('key: ' + key);
-      
+
       if (!fs.existsSync(localDestDir)) {
         fs.mkdirSync(localDestDir);
       }
@@ -101,12 +203,13 @@ export class S3UploaderSchedulerService {
           '(CURRENT) Remaining: ' +
             remainingFilesCurrentProcess +
             ' ( ' +
-            ((remainingFilesCurrentProcess / countFilesCurrentProcess) * 100).toFixed(2) +
+            (
+              (remainingFilesCurrentProcess / countFilesCurrentProcess) *
+              100
+            ).toFixed(2) +
             ' % )',
         );
-        this.logger.log(
-          '(OVERALL) Remaining: ' + remainingFiles,
-        );
+        this.logger.log('(OVERALL) Remaining: ' + remainingFiles);
       } catch (err) {
         this.logger.error('ERROR localSrcFile: ' + localSrcFile);
         this.logger.error(err);
